25971a06b953eefac3f4adf23a627291a8865656
sc8ing <16541352+sc8ing@users.noreply.github.com>
2019-01-31 17:51:05 -0500

    init

---
+++ npm front-end/index.js
@@ -0,0 +1,21 @@
+module.exports = class Pushkin {
+	constructor(con) {
+		this.connect(con);
+	}
+
+
+	connect(url, dbname, etc) {
+	}
+
+	prepExperimentRun() {
+	}
+
+	getAllStimuli() {
+	}
+
+	setSaveAfterEachStimulus(stimuli) {
+	}
+
+	endExperiment() {
+	}
+};
