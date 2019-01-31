a59f6dd63d60b095f5bb6d5aa2c5cf7ab60727ef
sc8ing <16541352+sc8ing@users.noreply.github.com>
2019-01-31 18:23:26 -0500

    basic methods

--- npm front-end/index.js
+++ npm front-end/index.js
@@ -1,21 +1,66 @@
+import Axios from 'axios';
+
 module.exports = class Pushkin {
-	constructor(con) {
-		this.connect(con);
+	constructor() {
+		this.con = undefined;
 	}
 
-
-	connect(url, dbname, etc) {
+	connect(quizAPIUrl) {
+		this.con = Axios.create({
+			baseURL: quizAPIUrl
+		});
 	}
+
+	loadScript(url) {
+		return new Promise( (resolve, reject) => {
+			setTimeout(() => reject(`Loading timed out for ${url}`), 5000);
+
+			// check if this script is already loaded and reload if it is
+			// can't use array 'has' because getElements doesn't return an array
+			const scripts = document.getElementsByTagName('script');
+			for (let i=0; i<scripts.length; i++) {
+				if (scripts[i].src == url)
+					scripts[i].parentNode.removeChild(scripts[i]);
+			}
+
+			const script = document.createElement('script');
+			script.onload = () => resolve;
+			script.src = url;
+			document.body.appendChild(script);
+		});
+
+	}
+
+	loadScripts(urls) { Promise.all(urls.map(loadScript)); }
 
 	prepExperimentRun() {
+		return this.con.post('/startExperiment');
 	}
 
 	getAllStimuli() {
+		return this.con.post('/getStimuli')
+			.then(res => {
+				const stimuli = res.data.resData;
+				return stimuli.map(s => JSON.parse(s.stimulus));
+			});
 	}
 
 	setSaveAfterEachStimulus(stimuli) {
+		return stimuli.map(s => ({
+			...stimTrial,
+			on_finish: this.saveStimulusResponse
+		}));
+	}
+
+	saveStimulusResponse(data) {
+		const postData = {
+			user_id: data.user_id,
+			data_string: data
+		};
+		return this.con.post('/stimulusResponse', postData);
 	}
 
 	endExperiment() {
+		return this.con.post('/endExperiment');
 	}
 };
