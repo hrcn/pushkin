b37530f87893d67884db15d45778aa62ed5f4e4b
sc8ing <16541352+sc8ing@users.noreply.github.com>
2019-02-07 17:18:46 -0500

    started class outline

--- pushkin-api/src/index.js
+++ pushkin-api/src/index.js
@@ -1,1 +1,20 @@
-console.log('hello');
+import express from 'express';
+import amqp from 'amqplib/callback_api';
+import uuid from 'uuid/v4';
+import cookieSession from 'cookie-session';
+import rpc from './rpc.js';
+
+export default class PushkinAPI {
+	constructor(expressPort, amqpAddress) {
+		this.expressPort = expressPort;
+		this.amqpAddress = amqpAddress;
+		this.app = express();
+		this.appStarted = false; // don't want to add controllers after the app has started
+	}
+
+	useController(controller, route) {
+		if (this.appStarted)
+			throw new Error('Pushkin API has already begun listening. Controllers must be added beforehand.');
+		this.app.use(
+	}
+}
