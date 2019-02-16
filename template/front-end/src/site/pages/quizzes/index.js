be5c155b60a3967e6f3c2fbd33f7bf4682c4baa4
sc8ing <16541352+sc8ing@users.noreply.github.com>
2019-02-16 15:52:31 -0500

    linked to myExperiments.js

--- template/front-end/src/site/pages/quizzes/index.js
+++ template/front-end/src/site/pages/quizzes/index.js
@@ -9,17 +9,16 @@
  */
 
 import React from 'react';
-import s from './styles.css';
 import { Route, Link } from 'react-router-dom';
 import Container from '../containers/container';
 /*
- * Some quizzes require a blank page. TakeQuiz simply loads the component
- * of the quiz that's requested and nothing else (e.g. no Container wrapper).
- * Designers of Pushkin quizzes can easily and explicitly choose exactly what
- * they want on their quiz page.
+ * Some experiments require a blank page. TakeQuiz simply loads the component
+ * of the experiment that's requested and nothing else (e.g. no Container wrapper).
+ * Designers of Pushkin experiments can easily and explicitly choose exactly what
+ * they want on their experiment page.
 */
 import TakeQuiz from './TakeQuiz.js';
-import quizzes from '../../quizzes/quizzes.js';
+import experiments from '../../../experiments/myExperiments.js';
 
 export default class QuizPage extends React.Component {
 	render() {
@@ -31,7 +30,7 @@ export default class QuizPage extends React.Component {
 					<p>Quizzes</p>
 					<ul>
 						{
-							Object.keys(quizzes).map( q =>
+							Object.keys(experiments).map( q =>
 								(
 									<li key={q}>
 										<Link to={`${match.url}/${q}`}>{q.split('/')[0]}</Link>
